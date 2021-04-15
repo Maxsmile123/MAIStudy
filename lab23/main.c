0b31135a03c7db918b8683ecbc48bdb09eaf30dc
Sysoev Maxim <54589783+Maxsmile123@users.noreply.github.com>
2021-04-15 20:57:48 +0300

    Update main.c

--- lab23/main.c
+++ lab23/main.c
@@ -3,7 +3,7 @@
 
 
 void print_menu() {
-	printf("Select the desired action by writing the appropriate number:\n");
+	printf("\nSelect the desired action by writing the appropriate number:\n");
 	printf("1) Add an item to the tree\n");
 	printf("2) Print the tree\n");
 	printf("3) Remove an item from the tree\n");
@@ -18,41 +18,39 @@ int main(void)
 	Tree *t = NULL;
 	int value;
 	char c;
-	printf("Welcome to the program for processing binary search trees!\n\n");
+	printf("Welcome to the program for processing binary search trees!\n");
 	print_menu();
 	while ((c = getchar()) != EOF) {
 		value = 0;
 		if (c == '\n') continue;
 		switch (c) {
 		case '1':
-			printf("Enter the value you want to add to the tree:");
+			printf("\nEnter the value you want to add to the tree:");
 			scanf_s("%d", &value);
-			if(t == NULL) t = tree_add_element(t, value);
-			else tree_add_element(t, value);
+			t = tree_add_element(t, value);
 			break;
 		case '2':
 			tree_print(t, 1);
 			break;
 		case '3':
-			printf("Enter the value you want to delete from the tree:");
+			printf("\nEnter the value you want to delete from the tree: ");
 			scanf_s("%d", &value);
-			delete_element(t, value);
+			t = delete_element(t, value);
 			break;
 		case '4':
-			if (is_AVL(t)) printf("Yes, your tree is AVL tree\n");
-			else printf("No, your tree doesn't AVL tree :( \n");
+			if (is_AVL(t)) printf("\nYes, your tree is AVL tree\n");
+			else printf("\nNo, your tree doesn't AVL tree :( \n");
 			break;
 		case '5':
-			printf("minimum element from your tree is %d\n", minimum(t)->data);
+			printf("\nminimum element from your tree is %d\n", minimum(t)->data);
 			break;
 		case '6':
-			printf("maximum element from your tree is %d\n", maximum(t)->data);
+			printf("\nmaximum element from your tree is %d\n", maximum(t)->data);
 			break;
 		case '0':
-			printf("Have a nice day!\n");
+			printf("\nHave a nice day!\n");
 			return 0;
 		default:
-			printf("%c\n", c);
 			printf("The number you entered isn't in the menu :(\nPlease try again, in case you made a mistake\n");
 			break;
 		}
