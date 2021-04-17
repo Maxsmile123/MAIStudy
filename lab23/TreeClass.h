0c7dbffe0fe2727d1d6b23b01a61364ed38d46ae
Sysoev Maxim <54589783+Maxsmile123@users.noreply.github.com>
2021-04-17 14:16:56 +0300

    Update TreeClass.h

--- lab23/TreeClass.h
+++ lab23/TreeClass.h
@@ -15,7 +15,7 @@ typedef struct
 Tree *tree_create(int value);
 Tree *tree_add_element(Tree *parent, int value);
 Tree *delete_element(Tree *parent, int value);
-void tree_print(Tree *t);
+void tree_print(Tree *t, int n);
 int is_AVL(Tree *t);
 Tree* minimum(Tree *t);
 Tree* maximum(Tree *t);
