9dcd2100c1f138a62a0f9926d7ea0f624644ae56
Sysoev Maxim <54589783+Maxsmile123@users.noreply.github.com>
2021-04-30 00:00:25 +0300

    Add files via upload

---
+++ kp6/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "structure.h"
+
+/*
+* Идея с именем файла. Заправшиваем один раз - если такого файла нет - создаём его. Если есть - ничего
+* Просто предоставляем доступ к функциям программы.
+    printf("Enter file name:\n");
+    scanf("%s", fileName);
+*/
+
+int main(int argc, char* argv[])
+{
+	
+    if (argc != 2) {
+        printf("Need exactly two arguments\n");
+        exit(1);
+    }
+    if (!strcmp(argv[1], "-a")) {
+        add();
+    }
+    else if (!strcmp(argv[1], "-p")) {
+        print();
+    }
+    else if (!strcmp(argv[1], "-d")) {
+        remove();
+    }
+    else if (!strcmp(argv[1], "-h")) {
+        print_help();
+    }
+    else if (!strcmp(argv[1], "-c")) {
+        create();
+    }
+    else if (!strcmp(argv[1], "-f")) {
+        typicalConfigurations();
+    }
+    else {
+        printf("Unknown command\n");
+    }
+    return 0;
+
+}
+
+void print_help() 
+{
+    printf("Welcome to the program for working with databases!\nYou have the following options:\n");
+    printf("1) Create the database(-c)\n 2) Add an entry to the database(-a)\n3) Print the database(-p)\n");
+    printf("4) Delete the database(-d)\n5) Print typical configurations of computers in a group(more than p owners) (-f)\n");
+}
---
+++ kp6/structure.h
@@ -0,0 +1,29 @@
+#ifndef structure_h
+#define structure_h
+
+#include <stdlib.h>
+
+typedef struct 
+{
+	char surname[50];
+	int numberOfProcessors;
+	char typeOfProcessors[5]; // 1 - x32, 2 - x64
+	int memoryCapacity;
+	char typeOfVideoController[10]; // 1 - встроенный, 2 - внешний, 3 - AGP, 4 - PCI
+	int videoMemoryCapacity;
+	char videoMemoryType[10]; // SCSI/IDE(1) or ATA/SATA(2)
+	int numberOfHardDrives;
+	int capacityOfHardDrives; // в сумме
+	int peripherals;
+	char OC[9]; // 1 - windows, 2 - linux, 3 - macOS
+} PC;
+
+void create();
+void add();
+void print();
+void remove();
+void typicalConfigurations();
+
+
+
+#endif
