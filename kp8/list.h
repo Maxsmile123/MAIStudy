d7992cc670c9c36c2995435755a3fb41acc1d239
Sysoev Maxim <54589783+Maxsmile123@users.noreply.github.com>
2021-05-16 15:17:14 +0300

    release version

--- kp8/list.h
+++ kp8/list.h
@@ -1,9 +1,11 @@
-#ifndef LIST.H
-#define LIST.H
+#ifndef LIST_H 
+#define LIST_H
+
 
 typedef char item;
+typedef struct _list_node list_node;
 
-typedef struct{
+typedef struct _list_node{
     item data;
     list_node *next;
 } list_node;
@@ -13,10 +15,10 @@ typedef struct{
 } list;
 
 list* listCreate(); // Done
-void listPrint(list *lst); // Done
+void listPrint(list *l); // Done
 void listInsert(list *lst, item data);
 void listRemove(list *lst, item data);
 int listLen(list *lst);
-void listReverse(list_node *lstFromHead, list_node *lstFromTail, int i, list *lst);
+void listReverse(list_node *lstFromHead, list_node *lstFromTail, int i, list *lst, int len);
 
 #endif
